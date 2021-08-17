1986287a3d8f90e1ec64d348a6ad2f26dfad61ac
David <itsdchen@gmail.com>
2021-08-17 17:48:36 -0400

    Update the livestart command.

--- backend/livestart.sh
+++ backend/livestart.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 export FLASK_APP=snobby_server.py
 export RUN_MODE='LIVE'
+export APP_CONFIG_FILE=../config/prod.py
 
-exec /home/ubuntu/miniconda3/bin/gunicorn app:app -b 0.0.0.0:8000
+exec /home/ubuntu/miniconda3/bin/gunicorn app:flask_app -b 0.0.0.0:8000
