a8c31ab8ba05d8de97ab7bc640a630dcacc0ba6f
David <itsdchen@gmail.com>
2021-08-25 14:07:41 -0400

    Made the ig parser more mature.

--- helper_tools/classify_igs/ig_parser.py
+++ helper_tools/classify_igs/ig_parser.py
@@ -22,6 +22,10 @@ postUrl,description,location,locationId,pubDate,isSidecar,type,caption,profileUr
 
 # This will probably change in the future, but still.
 
+TODO:
+- There's a real problem in detecting the partner. When there's a giveaway, sometimes they change the description to mention the winner first.
+- I'll have to deal with those. Ugh.
+
 """
 
 
@@ -38,30 +42,77 @@ def id_dishes(filepath):
 
 # All sequences of words that probably mean they're doing a partnership deal.
 promotion_keywords = [
-    'thank you to',
     'partnering with',
-    'from ',
+    'partnering ',
     'sending me',
     'sent me',
     'working with',
     'promo code',
     '% off',
     ' code ',
+    ' discount ',
     'giveaway',
 ]
 
-def id_promotions(list_of_dicts):
+def id_promotions(list_of_dicts, outfile):
     # OK, let's... do it.
 
-    #
+    kept_posts = []
+    for one_dict in list_of_dicts:
+        # Let's actually filter out the ones that don't make it, first.
+        lowercase_post = one_dict['description'].lower()
+        should_keep = False
+        for one_keyword in promotion_keywords:
+            if one_keyword in lowercase_post:
+                should_keep = True
+        # This means we probably identified a brand.
+        # OK, let's add it, but also print if it's not kept.
 
-    pass
+        # OK, right here, I should ID two things.
+        # the tagged_usernames are not consistent. But what seems to be true is that the
+        # first tagged thing is probably the brand or group in question.
+
+        first_tagged = ''
+        removed_newlines = ' '.join(lowercase_post.split('\n'))
+
+
+        # Find the first tagged thing.
+        at_loc = removed_newlines.find('@')
+        if at_loc >= 0:
+            short_newlines = removed_newlines[at_loc:]
+            short_newlines = short_newlines.replace(',', ' ')
+            potential_tag = short_newlines.split(' ')[0]
+            first_tagged = potential_tag
+
+        one_dict['inline_desc'] = removed_newlines
+        one_dict['potential_tag'] = first_tagged
+
+        if should_keep:
+            # Find the first tagged section.
+            kept_posts.append(one_dict.copy())
+        else:
+            pass
+
+    with open(outfile, 'w', newline='') as csvfile:
+        fieldnames = ['postUrl', 'location', 'username', 'tag', 'desc']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for one_post in kept_posts:
+            writer.writerow({'postUrl': one_post['postUrl'], 'location': one_post['location'], 'username': one_post['username'], 'tag': one_post['potential_tag'], 'desc': one_post['inline_desc']})
+    print("Done?")
+
+    return
+    for one_post in kept_posts:
+        print("*"*80)
+
+        print('{url}, {location}, {username}, {tag}, {desc}'.format(url=one_post['postUrl'], location=one_post['location'], username=one_post['username'], tag=one_post['potential_tag'], desc=one_post['inline_desc'] ))
 
 # List of urls that we processed already.
 # This is used for deduping.
 processed_links = set()
 
-wanted_cols = ['postUrl', 'description', 'location', 'isSidecar', 'profileUrl', 'username', 'fullName', 'imgUrl', 'taggedFullName1', 'taggedUsername1', 'taggedFullName2', 'taggedUsername2', 'taggedFullName3', 'taggedUsername3']
+wanted_cols = ['postUrl', 'description', 'location', 'isSidecar', 'profileUrl', 'username', 'fullName', 'imgUrl', 'taggedFullName1', 'taggedUsername1', 'taggedFullName2', 'taggedUsername2', 'taggedFullName3', 'taggedUsername3', 'taggedUsername4', 'taggedUsername5', 'taggedUsername6']
 #wanted_cols = ['location']
 
 def main():
@@ -70,6 +121,7 @@ def main():
 
     # first
     parser.add_argument('--infile', default='first_lines.csv')
+    parser.add_argument('--outfile', default='outfile.csv')
     # TODO: add options so they can select which function to call.
     args = parser.parse_args()
 
@@ -93,7 +145,7 @@ def main():
 
 
     # Now that we've gone and kept our links, let's pass these over to the promotion spotter...
-    id_promotions(kept_dicts)
+    id_promotions(kept_dicts, args.outfile)
     print("Done")
 
 
