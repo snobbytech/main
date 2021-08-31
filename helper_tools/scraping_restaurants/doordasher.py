4e1186e5edab37b672950f3e1f159938e73a9185
David <itsdchen@gmail.com>
2021-08-31 17:24:13 -0400

    More tmp progress in scraping doordash.

--- helper_tools/scraping_restaurants/doordasher.py
+++ helper_tools/scraping_restaurants/doordasher.py
@@ -21,58 +21,131 @@ from app.models.modelDefs import to_public_dict
 # Something to
 import requests
 
-# This part is switched between differen places.
-dd_menu_headers = {
-    'authority': 'www.doordash.com',
-    'x-channel-id': 'marketplace',
-    'apollographql-client-name': '@doordash/app-consumer-production-ssr',
-    'accept-language': 'en-US',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36',
-    'content-type': 'application/json',
-    'accept': '*/*',
-    'x-experience-id': 'doordash',
-    'apollographql-client-version': '1.0',
-    'x-csrftoken': '',
-    'origin': 'https://www.doordash.com',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    # This gets switched between different restaurants.
-#    'referer': 'https://www.doordash.com/store/woodhouse-fish-co-san-francisco-981727',
-    'cookie': 'x-device-id=268bd3a5-c19d-414d-b508-a4023b8c9a6d; __cf_bm=065c7c1d646a0d83302a8b4e547f13aae55217c6-1630417621-1800-AQPaHZ8LV7OyIYKhqpwCh8ZnrLg7GdIIDN2p/euxMDFG+kTIo2Iqau+eeCaUvhz/nFwKFPkzqGWxmaAIXAVKflOEJu/6CYMZnkuG8zAy5XMy; __cfruid=4653f5bddafbfca7c1badec5aaf2828cbf664ee2-1630417621; ajs_anonymous_id=%227d659388-c671-4e1d-a8fa-81d64f8e77d5%22',
-}
+# TODO: read this json, process and add each perdish thing.
+def parse_dish_addon():
+    # Given a json with a dish's addons, parse it...
 
-# This part is fixed.
-dd_menu_data = '${"operationName":"storepageFeed","variables":{"storeId":"981727","menuId":"","isMerchantPreview":false,"fulfillmentType":"Pickup","includeNestedOptions":true,"includeQuickAddContext":false},"query":"query storepageFeed($storeId: ID\\u0021, $menuId: ID, $isMerchantPreview: Boolean, $fulfillmentType: FulfillmentType, $includeNestedOptions: Boolean\\u0021, $includeQuickAddContext: Boolean\\u0021) {\\\\n storepageFeed(storeId: $storeId, menuId: $menuId, isMerchantPreview: $isMerchantPreview, fulfillmentType: $fulfillmentType) {\\\\n storeHeader {\\\\n id\\\\n name\\\\n description\\\\n priceRange\\\\n priceRangeDisplayString\\\\n offersDelivery\\\\n offersPickup\\\\n offersGroupOrder\\\\n isConvenience\\\\n isDashpassPartner\\\\n isShippingOnly\\\\n address {\\\\n lat\\\\n lng\\\\n city\\\\n street\\\\n displayAddress\\\\n cityLink\\\\n __typename\\\\n }\\\\n business {\\\\n id\\\\n name\\\\n link\\\\n differentialPricingEnabled\\\\n __typename\\\\n }\\\\n businessTags {\\\\n name\\\\n link\\\\n __typename\\\\n }\\\\n deliveryFeeLayout {\\\\n title\\\\n subtitle\\\\n isSurging\\\\n displayDeliveryFee\\\\n __typename\\\\n }\\\\n deliveryFeeTooltip {\\\\n title\\\\n description\\\\n __typename\\\\n }\\\\n coverImgUrl\\\\n coverSquareImgUrl\\\\n businessHeaderImgUrl\\\\n ratings {\\\\n numRatings\\\\n numRatingsDisplayString\\\\n averageRating\\\\n isNewlyAdded\\\\n __typename\\\\n }\\\\n savelists {\\\\n id\\\\n __typename\\\\n }\\\\n distanceFromConsumer {\\\\n value\\\\n label\\\\n __typename\\\\n }\\\\n enableSwitchToPickup\\\\n asapStatus {\\\\n unavailableStatus\\\\n displayUnavailableStatus\\\\n unavailableReason\\\\n displayUnavailableReason {\\\\n title\\\\n subtitle\\\\n __typename\\\\n }\\\\n isAvailable\\\\n unavailableReasonKeysList\\\\n __typename\\\\n }\\\\n asapPickupStatus {\\\\n unavailableStatus\\\\n displayUnavailableStatus\\\\n unavailableReason\\\\n displayUnavailableReason {\\\\n title\\\\n subtitle\\\\n __typename\\\\n }\\\\n isAvailable\\\\n unavailableReasonKeysList\\\\n __typename\\\\n }\\\\n status {\\\\n delivery {\\\\n isAvailable\\\\n minutes\\\\n operatingStatusEta {\\\\n value\\\\n unit\\\\n __typename\\\\n }\\\\n displayUnavailableStatus\\\\n unavailableReason\\\\n isTooFarFromConsumer\\\\n isStoreInactive\\\\n __typename\\\\n }\\\\n pickup {\\\\n isAvailable\\\\n minutes\\\\n displayUnavailableStatus\\\\n unavailableReason\\\\n isStoreInactive\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n isMenuV2\\\\n currency\\\\n travelTime {\\\\n value\\\\n type\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n banners {\\\\n pickup {\\\\n id\\\\n title\\\\n text\\\\n __typename\\\\n }\\\\n catering {\\\\n id\\\\n text\\\\n __typename\\\\n }\\\\n demandGen {\\\\n id\\\\n title\\\\n text\\\\n modals {\\\\n type\\\\n modalKey\\\\n modalInfo {\\\\n title\\\\n description\\\\n buttonsList {\\\\n text\\\\n action\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n demandTest {\\\\n id\\\\n title\\\\n text\\\\n modals {\\\\n type\\\\n modalKey\\\\n modalInfo {\\\\n title\\\\n description\\\\n buttonsList {\\\\n text\\\\n action\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n shipAnywhere {\\\\n id\\\\n title\\\\n text\\\\n modals {\\\\n type\\\\n modalKey\\\\n modalInfo {\\\\n title\\\\n description\\\\n buttonsList {\\\\n text\\\\n action\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n popup {\\\\n title\\\\n dismissButtonText\\\\n bulletCopyList\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n carousels {\\\\n id\\\\n type\\\\n name\\\\n description\\\\n items {\\\\n id\\\\n name\\\\n description\\\\n displayPrice\\\\n imgUrl\\\\n dynamicLabelDisplayString\\\\n calloutDisplayString\\\\n nextCursor\\\\n orderItemId\\\\n reorderCartId\\\\n reorderUuid\\\\n unitAmount\\\\n currency\\\\n ... @include(if: $includeNestedOptions) {\\\\n nestedOptions\\\\n __typename\\\\n }\\\\n specialInstructions\\\\n quickAddContext {\\\\n isEligible\\\\n price {\\\\n currency\\\\n decimalPlaces\\\\n displayString\\\\n sign\\\\n symbol\\\\n unitAmount\\\\n __typename\\\\n }\\\\n nestedOptions\\\\n specialInstructions\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n menuBook {\\\\n id\\\\n name\\\\n displayOpenHours\\\\n menuCategories {\\\\n id\\\\n name\\\\n numItems\\\\n next {\\\\n anchor\\\\n cursor\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n menuList {\\\\n id\\\\n name\\\\n displayOpenHours\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n itemLists {\\\\n id\\\\n name\\\\n description\\\\n items {\\\\n id\\\\n name\\\\n description\\\\n displayPrice\\\\n imageUrl\\\\n dynamicLabelDisplayString\\\\n calloutDisplayString\\\\n secondaryCallout {\\\\n logo\\\\n text\\\\n __typename\\\\n }\\\\n ... @include(if: $includeQuickAddContext) {\\\\n quickAddContext {\\\\n isEligible\\\\n price {\\\\n currency\\\\n decimalPlaces\\\\n displayString\\\\n sign\\\\n symbol\\\\n unitAmount\\\\n __typename\\\\n }\\\\n nestedOptions\\\\n specialInstructions\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n disclaimersList {\\\\n id\\\\n text\\\\n __typename\\\\n }\\\\n reviewPreview {\\\\n maxNumStars\\\\n consumerReviewData {\\\\n avgRating\\\\n numRatings\\\\n numRatingsDisplayString\\\\n insufficientRatings\\\\n numReviews\\\\n numReviewsDisplayString\\\\n consumerReviews {\\\\n consumerReviewUuid\\\\n reviewerDisplayName\\\\n starRating\\\\n reviewedAt\\\\n reviewText\\\\n isVerified\\\\n experience\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n popupContent {\\\\n id\\\\n type\\\\n title\\\\n message\\\\n dismissButtonText\\\\n metadataList {\\\\n key\\\\n value\\\\n __typename\\\\n }\\\\n bulletCopyList\\\\n showOnce\\\\n __typename\\\\n }\\\\n __typename\\\\n }\\\\n}\\\\n"}'
+    # This is a list of layers of options.
+    toret = []
+    dish_json_path = 'dd_dish_data.json'
+    dish_dict = {}
+    with open(dish_json_path, 'r') as f:
+        dish_dict = json.load(f)
+        dish_dict = dish_dict['data']['itemPage']
+        # The addons are in the optionsList
+    # dish price.
+    cur_rank = 0
+    for one_option_layer in dish_dict['optionsLists']:
+        cur_options = {}
+        cur_options['name'] = one_option_layer['name']
+        cur_options['rank'] = cur_rank
+        cur_options['min_options'] = one_option_layer['minNumOptions']
+        cur_options['max_options'] = one_option_layer['maxNumOptions']
+        all_options = []
+        for one_option in one_option_layer['options']:
+            cur_option_dict = {}
+            cur_option_dict['name']  = one_option['name']
+            cur_option_dict['price'] = one_option['unitAmount'] * 0.01
+            all_options.append(cur_option_dict)
+        cur_options['options'] = json.dumps(all_options)
+        cur_rank += 1
+        toret.append(cur_options)
+    # Now, return all of these things...
+    return toret
+
+# This part processes the doordash json obj.
+def parse_dd_rest_json():
+    rest_json_path = 'dd_rest_data.json'
+    rest_dict = {}
+    with open(rest_json_path, 'r') as f:
+        rest_dict = json.load(f)
+        rest_dict = rest_dict['data']['storepageFeed']
+
+    # I guess, now we can start playing around? Let's start populating the stuff...
+    # Then, we need to get the menu items:
+
+    final_rest_dict = {}
 
 
+    # Start parsing the stuff..
+    store_header_ = rest_dict['storeHeader']
+    # used for getting dish info.
+    store_id = store_header['id']
 
+    final_rest_dict['name'] = store_header['name']
+    # I guess no phone info.
+    # TODO: validate this.
+    final_rest_dict['delivery_options'] = 'DOORDASH'
+    final_rest_dict['available_pickup'] = store_header['offersPickup']
+    final_rest_dict['street_address']   = store_header['address']['street']
+    final_rest_dict['street_number']    = store_header['address']['street'].split()[0]
+    final_rest_dict['route']            = store_header['address']['street'].split()[1:]
+    # Not given
+    final_rest_dict['extra_address_id'] = ''
+    final_rest_dict['city']             = store_header['address']['city']
+    # Probably find the last two-digit thing
+    final_rest_dict['state'] = ''
+    # Not really necessary, I guess.
+    final_rest_dict['country'] = 'USA'
+    final_rest_dict['lat'] = store_header['address']['lat']
+    final_rest_dict['lon'] = store_header['address']['lng']
+
+    # This can be formatted differently.
+    final_rest_dict['hours'] = rest_dict['menuBook']['displayOpenHours']
+    final_rest_dict['hours_pickup'] = rest_dict['menuBook']['displayOpenHours']
+    # Not sure if this is necessary..
+    final_rest_dict['pickup_cutoff'] = ''
+
+
+    final_rest_dict['pickup_estimate_min'] = store_header['status']['pickup'].split('-')[0].strip()
+    final_rest_dict['pickup_estimate_max'] = store_header['status']['pickup'].split('-')[1].strip()
+    # I guess this is just relative to the state...
+    final_rest_dict['sales_tax_rate'] = ''
+    final_rest_dict['delivery_fee_taxable'] = ''
+    # IDK, but maybe we can just set this at $30?
+    final_rest_dict['order_minimum'] = 30
+    # We gotta know this.
+    final_rest_dict['dd_url'] =''
+
+    # Now, let's go through the itemList.
+    for one_itemlist in rest_dict['itemLists']:
+        itemlist_dict = {}
+        itemlist_dict['id'] = one_itemlist['id']
+        itemlist_dict['name'] = one_itemlist['name']
+        # Now we go through the dishes.
+        for one_dish in one_itemlist['items']:
+            dish_dict = {}
+            dish_dict['id'] = one_dish['id']
+            dish_dict['name'] = one_dish['name']
+            dish_dict['price'] = float(one_dish['displayPrice'].replace('$', ''))
+
+            # We should get the addon too.
+            # TODO: add this dish to the thinger.
+            pass
+
+
+    print(rest_dict)
+
+    pass
+
+# This part scrapes and extracts the json from a doordash menu.
 def grab_doordash_main(restaurant_url):
     restaurant_header = dd_menu_headers.copy()
 
     # Process the url in case there are other args.
     restaurant_url = restaurant_url.split('?')[0]
-
     # OK, now give the referrer stuff.
     restaurant_header['referer'] = restaurant_url
 
-    # OK, now make the request
-    dd_response = requests.post('https://www.doordash.com/graphql', headers=restaurant_header, data=dd_menu_data)
-    if dd_response.status_code != 200:
-        print("Something broke when trying to grab {}, exiting".format(restaurant_url))
-        print(dd_response.status_code)
-        sys.exit()
-    # Otherwise, parse the json
-
-    js_obj = json.loads(dd_response.text)
-    # Now, let's just print it?
-    print(js_obj)
+    # TODO: copy the stuff from the
 
     pass
 
 def main():
-    the_url = 'https://www.doordash.com/store/woodhouse-fish-co-san-francisco-981727/'
-    grab_doordash_main(the_url)
+    #the_url = 'https://www.doordash.com/store/woodhouse-fish-co-san-francisco-981727/'
+    #grab_doordash_main(the_url)
+
+    rest_obj = parse_dd_rest_json()
 
     pass
 
